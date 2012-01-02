4ccbca52fcdac114f0d99be3d8160084c08bea06
Tim Wingfield <tim@timwingfield.com>
2012-01-02 12:41:45 -0500

    added some server tests

--- test/app_server_test.rb
+++ test/app_server_test.rb
@@ -2,11 +2,14 @@ ENV['RACK_ENV'] = 'test'
 
 $: << File.dirname(__FILE__) + "/../lib"
 
+require "sinatra/base"
 require "minitest/autorun"
 require "rack/test"
 require "mongo"
 
 require "server"
+
+Barista.logger.level = Logger::WARN
 
 module DBUtils
   def drop_db
@@ -16,5 +19,25 @@ module DBUtils
 end
 
 describe AppServer do
+  
+  include Rack::Test::Methods
+  include DBUtils
 
+  def app
+    AppServer.new
+  end
+
+  describe "/" do
+    before do
+      get "/"
+    end
+
+    it "should respond with success" do
+      assert_equal 200, last_response.status
+    end
+
+    it "should emit an HTML header" do
+      assert_equal "text/html;charset=utf-8", last_response.headers["Content-type"]
+    end
+  end
 end
